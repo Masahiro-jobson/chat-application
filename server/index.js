53c0553e9ed8f62cbd65ed26fd1870e3188f3114
masahiro <m.togasaki1026@gmail.com>
2025-12-12 11:35:43 +1100

    First Commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+// We need three variables to build server together with a socket.io
+const express = require("express");
+const app = express();
+const http = require("http");
+// cors are important for socket.io because it handles a lot of cors issue
+const cors = require("cors");
+const {Server} = require("socket.io");
+
+// This function resolve many of cors issues.
+app.use(cors());
+// This variable is for creating server.
+const server = http.createServer(app);
+
+const io = new Server(server, {
+// cors = a mechanism that allows web applications to access resources from different domains
+    cors: {
+// origin tells our server which url (server) is called and making a call on our socke.io server
+        origin: "http://localhost:3000/",
+        methods: ["GET", "POST"],
+
+    },
+});
+
+// io.on listens certain event in the bracket.
+// connection is the event that detects if someone connected to this socket io server.
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+// It runs automatically when the user is gone.
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+
+    });
+
+});
+
+
+server.listen(3001, () => {
+    console.log("SERVER RUNNING");
+})
